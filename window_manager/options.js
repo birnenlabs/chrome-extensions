30309d9793e10cc4ee7d500b154321bc5d33da16
Lukasz Gruszka <lukasz>
2024-03-08 12:05:43 +0100

    use real validated objects as examples

--- window_manager/options.js
+++ window_manager/options.js
@@ -1,6 +1,7 @@
 import {filterWithDisplay, matchActionsToDisplay, Action, ActionWithDisplay} from './classes/action.js';
 import {Displays} from './classes/displays.js';
 import {matchMatcherToAction, Matcher, MatcherWithAction} from './classes/matcher.js';
+import {Position} from './classes/position.js';
 import {Storage} from './classes/storage.js';
 import {checkNonUndefined} from './utils/preconditions.js';
 import {combine2} from './utils/promise.js';
@@ -384,6 +385,45 @@ function onPageLoad() {
   actionsEditor = createJSONEditorForElement('actionsInput', [], schemaValidator.actions);
   matchersEditor = createJSONEditorForElement('matchersInput', [], schemaValidator.matchers);
   settingsEditor = createJSONEditorForElement('settingsInput', {}, schemaValidator.settings);
+
+
+  const actionExample = new Action();
+  actionExample.comment = 'An example Action definition';
+  actionExample.id = 'action-id';
+  actionExample.display = 'display-name';
+  actionExample.column = new Position();
+  actionExample.column.start = 0;
+  actionExample.column.end = '100%';
+  actionExample.row = new Position();
+  actionExample.row.start = 0;
+  actionExample.row.end = '100%';
+  actionExample.shortcutId = 0;
+  actionExample.menuName = '';
+
+  const matcherExample = new Matcher();
+  matcherExample.comment = 'An example Matcher definition';
+  matcherExample.actions = ['action-id'];
+  matcherExample.windowTypes = ['normal'];
+  matcherExample.anyTabUrl = 'https://example.org/';
+  matcherExample.minTabsNum = 0;
+  matcherExample.maxTabsNum = 1000000;
+
+  const actionExampleEl = checkNonUndefined(document.getElementById('actionExample'));
+  const matcherExampleEl = checkNonUndefined(document.getElementById('matcherExample'));
+
+  try {
+    actionExample.validate();
+    actionExampleEl.textContent = JSON.stringify(actionExample, undefined, 2);
+  } catch (e) {
+    actionExampleEl.textContent = 'Invalid example (most likely bug in the code):\n' + e.message;
+  }
+  try {
+    matcherExample.validate();
+    matcherExampleEl.textContent = JSON.stringify(matcherExample, undefined, 2);
+  } catch (e) {
+    matcherExampleEl.textContent = 'Invalid example (most likely bug in the code):\n' + e.message;
+  }
+
 
   return Storage.getRawConfiguration().then((config) => {
     actionsEditor.set({text: config.actions});
