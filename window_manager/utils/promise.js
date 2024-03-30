17ca86094dce176593362a350a03b89531f328d3
Lukasz Gruszka <lukasz>
2024-03-30 09:11:28 +0100

    Allow non promise values in combine

--- window_manager/utils/promise.js
+++ window_manager/utils/promise.js
@@ -5,8 +5,8 @@
  * @template P2
  * @template R
  *
- * @param {Promise<P1>} promise1
- * @param {Promise<P2>} promise2
+ * @param {P1|Promise<P1>} promise1
+ * @param {P2|Promise<P2>} promise2
  * @param {function(P1, P2): R|Promise<R>} combineFn
  * @return {Promise<R>}
  */
@@ -22,9 +22,9 @@ export function combine2(promise1, promise2, combineFn) {
  * @template P3
  * @template R
  *
- * @param {Promise<P1>} promise1
- * @param {Promise<P2>} promise2
- * @param {Promise<P3>} promise3
+ * @param {P1|Promise<P1>} promise1
+ * @param {P2|Promise<P2>} promise2
+ * @param {P3|Promise<P3>} promise3
  * @param {function(P1, P2, P3): R|Promise<R>} combineFn
  * @return {Promise<R>}
  */
@@ -41,10 +41,10 @@ export function combine3(promise1, promise2, promise3, combineFn) {
  * @template P4
  * @template R
  *
- * @param {Promise<P1>} promise1
- * @param {Promise<P2>} promise2
- * @param {Promise<P3>} promise3
- * @param {Promise<P4>} promise4
+ * @param {P1|Promise<P1>} promise1
+ * @param {P2|Promise<P2>} promise2
+ * @param {P3|Promise<P3>} promise3
+ * @param {P4|Promise<P4>} promise4
  * @param {function(P1, P2, P3, P4): R|Promise<R>} combineFn
  * @return {Promise<R>}
  */
