f347d7ce4cddaeec080c5af0e66e90a0e1130524
Lukasz Gruszka <lukasz>
2024-05-08 10:47:59 +0200

    Use minute timestamp, refactor to remove async code

--- status_bar_helper/background.js
+++ status_bar_helper/background.js
@@ -1,32 +1,71 @@
 import {FirebaseSettings} from './firebase-settings.js';
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
-import {getDatabase, ref, set} from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
+import {Database, getDatabase, ref, set} from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
 
 
 /**
  * @typedef {Object} SongInfo
  * @property {string} artist
  * @property {string} title
- * @property {number} timestamp
+ * @property {number} timestampMin
+ */
+
+/**
+ * @typedef {Object} DbData
+ * @property {string|undefined} token
+ * @property {Database|undefined} db
+ */
+
+/**
+ * @typedef {Object} VerifiedDbData
+ * @property {string} token
+ * @property {Database} db
  */
 
 const KEY_SONG_TITLE = 'song-title';
 
+/**
+ * @return {Promise<DbData>}
+ */
+function createDbData() {
+  console.log('Creating database');
+  const tokenPromise = FirebaseSettings.getToken();
+  const dbPromise = FirebaseSettings.getUrl()
+      .then((databaseURL) => databaseURL ? getDatabase(initializeApp({databaseURL})) : undefined);
+
+  return Promise.all([tokenPromise, dbPromise])
+      .then((vals) => ({token: vals[0], db: vals[1]}));
+}
+
+/**
+ * @param {DbData} dbData
+ * @return {VerifiedDbData}
+ */
+function verifyDbData(dbData) {
+  if (dbData.token && dbData.db) {
+    return ( /** @type {VerifiedDbData} */(dbData));
+  } else {
+    throw new Error('DbData invalid');
+  }
+}
+
+/** @type{Promise<VerifiedDbData>|undefined} */
+let dbDataPromise = undefined;
 
 /**
  * @param {SongInfo} songInfo
+ * @return {Promise<any>}
  */
-async function setInDatabase(songInfo) {
+function setInDatabase(songInfo) {
   console.log('Sending song info to firebase:', songInfo);
-  const databaseURL = await FirebaseSettings.getUrl();
-  const token = await FirebaseSettings.getToken();
-  if (!databaseURL) {
-    console.warn('Database url not set');
-    return;
-  }
 
-  const db = getDatabase(initializeApp({databaseURL}));
-  set(ref(db, token + '/songs/current'), songInfo);
+  const verifiedDbDataPromise = (dbDataPromise || createDbData().then(verifyDbData));
+
+  return verifiedDbDataPromise
+    // Database promise will be cached as dbDataPromise only if there was no error when creating db.
+    .then(() => dbDataPromise = verifiedDbDataPromise)
+    .then((dbData) => set(ref(dbData.db, dbData.token + '/songs/current'), songInfo))
+    .catch((e) => console.log(e));
 }
 
 
--- status_bar_helper/content_script_music.js
+++ status_bar_helper/content_script_music.js
@@ -8,35 +8,40 @@ console.log('Status bar helper loaded, setting media handlers');
 
 const CHECK_REPEAT_MS = 2000;
 
+/** @type {SongInfo} */
+const EMPTY_INFO = {
+      artist: '',
+      title: '',
+      timestampMin: 0,
+    };
+
+/**
+ * @return {SongInfo}
+ */
+function getInfo() {
+  if (navigator.mediaSession?.playbackState === 'playing') {
+    const metadata = window.navigator?.mediaSession?.metadata;
+    return {
+      artist: metadata?.artist || '',
+      title: metadata?.title || '',
+      timestampMin: Math.floor(Date.now() / 60000),
+    };
+  } else {
+    return EMPTY_INFO;
+  }
+}
+
 /**
  * @return {Promise<any>}
  */
 function handleStateChange() {
-  const metadata = (navigator.mediaSession?.playbackState === 'playing') ?
-    window.navigator?.mediaSession?.metadata : undefined;
+  const info = getInfo();
 
-  /** @type {SongInfo} */
-  const info = {
-    artist: metadata?.artist || '',
-    title: metadata?.title || '',
-    timestamp: Date.now(),
-  };
-
-
-  return chrome.storage.session.get(KEY_SONG_TITLE)
-  // Store in session only if different
-      .then((stored) =>
-      shouldUpdate(stored[KEY_SONG_TITLE], info) ? chrome.storage.session.set({[KEY_SONG_TITLE]: info}) : undefined)
+  // Update everytime for now - assuming 24h/day music this will end up
+  // with 166MB of transfer per month (out of 10GB limit).
+  // When the object is not changed, the onChange event will not be fired for the storage.
+  return chrome.storage.session.set({[KEY_SONG_TITLE]: info})
       .then(() => setTimeout(handleStateChange, CHECK_REPEAT_MS));
-}
-
-/**
- * @param {SongInfo|undefined} o1
- * @param {SongInfo} o2
- * @return {boolean}
- */
-function shouldUpdate(o1, o2) {
-  return o1?.title !== o2.title || o1?.artist !== o2.artist || Math.abs(o1?.timestamp - o2.timestamp) > 120000;
 }
 
 handleStateChange();
