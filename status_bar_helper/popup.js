65f8c3b3abbcbaf9c3aec8b6cd2b29f9534a2f8f
Lukasz Gruszka <lukasz>
2024-05-10 16:07:27 +0200

    use common lib

--- status_bar_helper/popup.js
+++ status_bar_helper/popup.js
@@ -1,32 +1,21 @@
 import {FirebaseSettings} from './firebase-settings.js';
-
-/**
- * @param{string} id
- * @return {HTMLInputElement}
- */
-function getInput(id) {
-  const inputEl = document.getElementById(id);
-  if (inputEl instanceof HTMLInputElement) {
-    return inputEl;
-  }
-  throw new Error(`Input ${id} not found.`);
-}
+import {getHTMLInputElement} from './jslib/js/html.js';
 
 /**
  * @return {Promise<any>}
  */
 function firebaseInit() {
   return FirebaseSettings.getUrl()
-      .then((url) => getInput('firebase-url').value = (url || ''))
+      .then((url) => getHTMLInputElement('firebase-url').value = (url || ''))
       .then(() => FirebaseSettings.getToken())
-      .then((token) => getInput('firebase-token').value = (token || ''));
+      .then((token) => getHTMLInputElement('firebase-token').value = (token || ''));
 }
 
 /**
  * @return {Promise<any>}
  */
 function firebaseSave() {
-  return FirebaseSettings.save(getInput('firebase-url').value, getInput('firebase-token').value);
+  return FirebaseSettings.save(getHTMLInputElement('firebase-url').value, getHTMLInputElement('firebase-token').value);
 }
 
 
